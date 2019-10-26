9da779d49377a163cf9b33db3a825d9d4c509680
Pape Samba DIALLO <papesd2@gmail.com>
2019-10-26 13:26:33 +0200

    mes n-iémes premiers pas en python

--- exo.py
+++ exo.py
@@ -1,37 +1,78 @@
 #!/usr/bin/env python
-
+# -*- coding:utf-8 -*-
 
 #print "Hello World"
+ 
 
 
-def somme2():
+def somme2(a, b):
     """
      Fait la somme de deux entiers
     """
+    c = a + b
 
-def somme4():
+    return c
+
+
+
+p = somme2(1, 2)
+print(p)
+
+
+def somme4(m, n, h, f):
     """
     Fait la somme de quatre entiers en se basant sur la fonction somm2()
     """
+    d = somme2(somme2(m,n), somme2(h,f))
+    return d
 
-estPaire = lambda : # Fonction lambda qui retourne si un nombre est paire ou impaire
+def somme3(w, x, r):
+    d = somme2(somme2(w, x), r)
+    return d
 
 
-def sommeGeneral():
+v = somme4(1,4, 3, 2)
+print(v)
+
+#estPaire = lambda x : (True if (x%2 == 0) else False)
+
+estPaire = lambda x :(x%2 == 0)
+
+
+
+# Fonction lambda qui retourne si un nombre est paire ou impaire
+
+def sommeGeneral(*arg):
     """
-    Fait la somme de n nombres données en paramètre (n étant variable)
+    Fait la somme de n nombres donnees en parametre (n etant variable)
     """
 
+    s = 0
+    for i in arg:
+        s = s + i
+    return s
 
+s = sommeGeneral(10, 20, 30)
+
+print(s)
 
 x = (2, 4)
 y = (3, 4)
 
-
-def distance():
+import math
+def distance(a, b):
     """
-    Calcule la distance entre deux points. Le deuxième point doit être un paramètre facultatif, par défaut il vaut (0,0)
+    Calcule la distance entre deux points. Le deuxieme point doit etre un parametre facultatif, par defaut il vaut (0,0)
     """
 
+    q = math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+    return q
+
+x = (3, 4)
+y = (3, 4)
+
+q = distance(x, y)
+print(q)
 
 
