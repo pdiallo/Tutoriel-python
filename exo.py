a30a8460449674cd2083391c8604697a4ac1dfb5
skante <souleymanecheickkante@yahoo.fr>
2019-10-26 11:08:09 +0200

    commit

---
+++ exo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+#print "Hello World"
+
+
+def somme2():
+    """
+     Fait la somme de deux entiers
+    """
+
+def somme4():
+    """
+    Fait la somme de quatre entiers en se basant sur la fonction somm2()
+    """
+
+estPaire = lambda : # Fonction lambda qui retourne si un nombre est paire ou impaire
+
+
+def sommeGeneral():
+    """
+    Fait la somme de n nombres données en paramètre (n étant variable)
+    """
+
+
+
+x = (2, 4)
+y = (3, 4)
+
+
+def distance():
+    """
+    Calcule la distance entre deux points. Le deuxième point doit être un paramètre facultatif, par défaut il vaut (0,0)
+    """
+
+
+
